c4d9b9a87fe60bc7817898c4e7097bd43b7dccb1
Fabian Stäber <fabian@fstab.de>
2015-09-03 16:43:06 +0200

    [undertow-http2-servlet-example]: large GET requests and POST for undertow servlet example

--- undertow-http2-servlet-example/src/main/java/de/consol/labs/h2c/HelloWorldServlet.java
+++ undertow-http2-servlet-example/src/main/java/de/consol/labs/h2c/HelloWorldServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -22,7 +23,35 @@ public class HelloWorldServlet extends HttpServlet {
         int reqNr = ((AtomicInteger) session.getAttribute("counter")).incrementAndGet();
         PrintWriter writer = resp.getWriter();
         writer.write("Hello, World!\n");
-        writer.write("Btw, this is request number " + reqNr + ".");
+        writer.write("Btw, this is request number " + reqNr + ".\n");
+        int size = readSize(req);
+        if (size > 0) {
+            writer.write("Here are " + size + " 'a' characters\n");
+            for (int i = 1; i <= size; i++) {
+                writer.write("a");
+                if (i % 80 == 0) {
+                    writer.write("\n");
+                }
+            }
+        }
+        writer.close();
+    }
+
+    private int readSize(HttpServletRequest request) {
+        if (request.getParameterMap().containsKey("size")) {
+            return Integer.parseInt(request.getParameter("size"));
+        }
+        return 0;
+    }
+
+    // Mapped to https://localhost:8443/hello-world/api/hello-world
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+        String body = reader.readLine();
+
+        PrintWriter writer = resp.getWriter();
+        writer.write("Receives \"" + body + "\"\n");
         writer.close();
     }
 }
